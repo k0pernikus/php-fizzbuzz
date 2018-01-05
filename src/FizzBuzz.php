46bccdd2ce818e87252b35c64bee242f4aff1988
Philipp Kretzschmar <philipp.kretzschmar@gmail.com>
2018-01-05 10:40:58 +0100

    setup the unit tests

---
+++ src/FizzBuzz.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kopernikus\FizzBuzz;
+
+/**
+ * FizzBuzz
+ **/
+class FizzBuzz
+{
+    /**
+     * @param int $n
+     * @return String
+     */
+    public function fizzBuzz(int $n): String
+    {
+        return "";
+    }
+}
---
+++ tests/Kopernikus/FizzBuzz/FizzBuzzTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: philipp
+ * Date: 05.01.18
+ * Time: 10:24
+ */
+
+namespace Kopernikus\FizzBuzz;
+
+
+class FizzBuzzTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider fizzBuzzProvider
+     */
+    public function testFizzBuzz(int $input, string $expected): void
+    {
+        $fizzBuzz = new FizzBuzz();
+        $actual = $fizzBuzz->fizzBuzz($input);
+        $this->assertEquals($expected, $actual, "The input ${input} should be converted to the proper string ${expected}");
+    }
+
+    public function fizzBuzzProvider()
+    {
+        return [
+            [2, '2'],
+            [13, '13'],
+            [3, 'Fizz'],
+            [6, 'Fizz'],
+            [5, 'Buzz'],
+            [10, 'Buzz'],
+            [15, 'FizzBuzz'],
+            [30, 'FizzBuzz'],
+        ];
+    }
+
+}
